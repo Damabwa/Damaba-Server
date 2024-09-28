601f40707cd8b4235d05d8ca7b79ce59b2e9b7b3
JaeUk <siwall0105@gmail.com>
2024-09-28 19:17:52 +0900

    chore: user-domain module에 `spring-context`, `spring-tx` 의존성 추가

--- user/user-domain/build.gradle.kts
+++ user/user-domain/build.gradle.kts
@@ -1,3 +1,8 @@
+dependencies {
+    compileOnly("org.springframework:spring-context")
+    compileOnly("org.springframework:spring-tx")
+}
+
 tasks.bootJar { enabled = false }
 
 tasks.jar { enabled = true }
