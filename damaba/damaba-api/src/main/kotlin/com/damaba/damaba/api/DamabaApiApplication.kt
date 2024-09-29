85e6236196b072fecfa46f4162e27eca86ce7fe4
JaeUk <siwall0105@gmail.com>
2024-09-29 19:35:45 +0900

    chore(`damaba-api`): `scanBasePackages` 수정

--- damaba/damaba-api/src/main/kotlin/com/damaba/damaba/api/DamabaApiApplication.kt
+++ damaba/damaba-api/src/main/kotlin/com/damaba/damaba/api/DamabaApiApplication.kt
@@ -3,7 +3,16 @@ package com.damaba.damaba.api
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = [
+        "com.damaba.common_logging",
+        "com.damaba.common_exception",
+        "com.damaba.damaba.api",
+        "com.damaba.user.api",
+        "com.damaba.user.domain",
+        "com.damaba.user.infra",
+    ],
+)
 class DamabaApiApplication
 
 fun main(args: Array<String>) {
