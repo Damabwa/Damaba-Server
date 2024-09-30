34a9ff488fa8f9d9d8f0884a36c16fa2b0ef2979
JaeUk <siwall0105@gmail.com>
2024-10-01 05:02:52 +0900

    chore: #1 user table 정의

---
+++ damaba/damaba-api/src/main/resources/schema.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user`
+(
+    id               BIGINT       NOT NULL AUTO_INCREMENT,
+    o_auth_login_uid VARCHAR(255) NOT NULL,
+    login_type       VARCHAR(255) NOT NULL,
+    created_at       DATETIME     NOT NULL,
+    updated_at       DATETIME     NOT NULL,
+    deleted_at       DATETIME,
+    PRIMARY KEY (id)
+)
