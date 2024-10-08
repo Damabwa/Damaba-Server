b36931dc000fdbf55eeaece8c7c17a1f76c00415
JaeUk <siwall0105@gmail.com>
2024-10-08 21:24:21 +0900

    chore: #1 jwt 관련 의존성(jjwt) 추가

--- user/build.gradle.kts
+++ user/build.gradle.kts
@@ -41,6 +41,11 @@ dependencies {
     /**
      * Global
      */
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
