eeab553339d4b80d9d131c1be9655f97cfca08dc
JaeUk <siwall0105@gmail.com>
2024-10-08 16:11:10 +0900

    refactor: `damaba-api` 모듈을 상위 모듈인 `damaba`로 통합

--- damaba/build.gradle.kts
+++ damaba/build.gradle.kts
@@ -6,53 +6,78 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
 }
 
-allprojects {
-    group = "com.damaba.damaba"
-    version = "0.0.1"
+group = "com.damaba.damaba"
+version = "0.0.1"
 
-    repositories {
-        mavenCentral()
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
     }
 }
 
-subprojects {
-    apply(plugin = "kotlin")
-    apply(plugin = "kotlin-spring")
-    apply(plugin = "org.springframework.boot")
-    apply(plugin = "io.spring.dependency-management")
-    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+dependencies {
+    /**
+     * Main Server Dependencies
+     */
+    implementation(project(":user"))
 
-    java {
-        toolchain {
-            languageVersion = JavaLanguageVersion.of(17)
-        }
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+
+    // Swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.6.0")
+
+    /**
+     * Controller(API)
+     */
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    /**
+     * Infrastructure
+     */
+    // JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // RDB
+    implementation("com.h2database:h2")
+    implementation("com.mysql:mysql-connector-j")
+
+    /**
+     * Global
+     */
+    implementation(project(":common-logging"))
+    implementation(project(":common-exception"))
+
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
     }
+}
 
-    dependencies {
-        implementation("org.springframework.boot:spring-boot-starter")
-        implementation("org.jetbrains.kotlin:kotlin-reflect")
-        testImplementation("org.springframework.boot:spring-boot-starter-test")
-        testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    }
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
 
-    kotlin {
-        compilerOptions {
-            freeCompilerArgs.addAll("-Xjsr305=strict")
-        }
-    }
-
-    tasks.withType<Test> {
-        useJUnitPlatform()
-    }
-
-    ktlint {
-        android.set(false)
-        outputToConsole.set(true)
-        ignoreFailures.set(false)
-        filter {
-            exclude("**/generated/**")
-        }
+ktlint {
+    android.set(false)
+    outputToConsole.set(true)
+    ignoreFailures.set(false)
+    filter {
+        exclude("**/generated/**")
     }
 }
 
