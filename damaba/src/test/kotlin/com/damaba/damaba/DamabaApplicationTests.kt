76f8ab4e53b36f4a2493072babbf2b7438898065
JaeUk <siwall0105@gmail.com>
2024-10-08 21:23:55 +0900

    test: #1 `contextLoads()` test의 profile을 "test"로 설정

--- damaba/src/test/kotlin/com/damaba/damaba/DamabaApplicationTests.kt
+++ damaba/src/test/kotlin/com/damaba/damaba/DamabaApplicationTests.kt
@@ -2,9 +2,11 @@ package com.damaba.damaba
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
+@ActiveProfiles("test")
 @SpringBootTest
-class DamabaApiApplicationTests {
+class DamabaApplicationTests {
 
     @Test
     fun contextLoads() {
