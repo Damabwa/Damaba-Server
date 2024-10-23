a8f970d7b2544d1df77a627fb00631bde4b3153d
JaeUk <siwall0105@gmail.com>
2024-10-24 01:32:26 +0900

    chore(damaba): #24 thread pool 설정 추가

---
+++ damaba/src/main/kotlin/com/damaba/damaba/config/AsyncConfig.kt
@@ -0,0 +1,24 @@
+package com.damaba.damaba.config
+
+import com.damaba.common_logging.MdcThreadPoolTaskDecorator
+import com.damaba.damaba.property.ThreadPoolProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+
+@Configuration
+@EnableAsync
+class AsyncConfig {
+    @Bean
+    fun threadPoolTaskExecutor(threadPoolProperties: ThreadPoolProperties): ThreadPoolTaskExecutor {
+        val executor = ThreadPoolTaskExecutor()
+        executor.corePoolSize = threadPoolProperties.corePoolSize
+        executor.maxPoolSize = threadPoolProperties.maxPoolSize
+        executor.queueCapacity = threadPoolProperties.queueCapacity
+        executor.setTaskDecorator(MdcThreadPoolTaskDecorator())
+        executor.setThreadNamePrefix("damaba-async-")
+        executor.initialize()
+        return executor
+    }
+}
--- damaba/src/main/kotlin/com/damaba/damaba/config/LogTraceAspect.kt
+++ damaba/src/main/kotlin/com/damaba/damaba/config/LogTraceAspect.kt
@@ -12,18 +12,19 @@ import org.springframework.stereotype.Component
 @Aspect
 class LogTraceAspect(private val logTrace: LogTrace) {
     @Around(
-        value = "com.damaba.damaba.config.Pointcuts.controllerPoint() || " +
-            "com.damaba.damaba.config.Pointcuts.servicePoint() || " +
-            "com.damaba.damaba.config.Pointcuts.repositoryPoint()",
+        value = "com.damaba.damaba.config.Pointcuts.controllerPointcuts() || " +
+            "com.damaba.damaba.config.Pointcuts.useCasePointcuts() || " +
+            "com.damaba.damaba.config.Pointcuts.domainServicePointcuts() || " +
+            "com.damaba.damaba.config.Pointcuts.repositoryPointcuts() || " +
+            "com.damaba.damaba.config.Pointcuts.infrastructureServicePointcuts() || " +
+            "com.damaba.damaba.config.Pointcuts.eventListenerPointcuts()",
     )
     fun execute(joinPoint: ProceedingJoinPoint): Any? {
         var status: TraceStatus? = null
         try {
             val message = joinPoint.signature.toShortString()
             status = logTrace.begin(message)
-
             val result: Any? = joinPoint.proceed()
-
             logTrace.end(status)
             return result
         } catch (ex: Exception) {
@@ -36,15 +37,27 @@ class LogTraceAspect(private val logTrace: LogTrace) {
 }
 
 class Pointcuts {
-    @Pointcut("@within(org.springframework.stereotype.Controller) || @within(org.springframework.web.bind.annotation.RestController)")
-    fun controllerPoint() {
+    @Pointcut("execution(* com.damaba..controller..*Controller.*(..))")
+    fun controllerPointcuts() {
     }
 
-    @Pointcut("@within(org.springframework.stereotype.Service)")
-    fun servicePoint() {
+    @Pointcut("execution(* com.damaba..application..*UseCase.*(..))")
+    fun useCasePointcuts() {
     }
 
-    @Pointcut("@within(org.springframework.stereotype.Repository)")
-    fun repositoryPoint() {
+    @Pointcut("execution(* com.damaba..domain..*Service.*(..))")
+    fun domainServicePointcuts() {
+    }
+
+    @Pointcut("execution(* com.damaba..infrastructure..*Repository.*(..))")
+    fun repositoryPointcuts() {
+    }
+
+    @Pointcut("execution(* com.damaba..infrastructure..*Service.*(..))")
+    fun infrastructureServicePointcuts() {
+    }
+
+    @Pointcut("execution(* com.damaba..infrastructure..*EventListener.*(..))")
+    fun eventListenerPointcuts() {
     }
 }
