b2f34836a060d219a1623a58b6ebe7a338c621eb
JaeUk <siwall0105@gmail.com>
2024-10-28 05:07:57 +0900

    refactor: #18 event publisher 구현체를 user module에서 damaba 모듈로 이동

--- damaba/src/main/kotlin/com/damaba/damaba/adapter/outbound/common/SpringEventPublisher.kt
+++ damaba/src/main/kotlin/com/damaba/damaba/adapter/outbound/common/SpringEventPublisher.kt
@@ -1,13 +1,15 @@
-package com.damaba.user.adapter.outbound.common
+package com.damaba.damaba.adapter.outbound.common
 
-import com.damaba.user.application.port.outbound.common.PublishEventPort
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Component
+
+// 추후, 분산서버로 전환 시 각 서버에 event publisher 구현체 별도 구현
 
 @Component
 class SpringEventPublisher(
     private val applicationEventPublisher: ApplicationEventPublisher,
-) : PublishEventPort {
+) : com.damaba.user.application.port.outbound.common.PublishEventPort,
+    com.damaba.damaba.application.port.outbound.common.PublishEventPort {
     override fun publish(event: Any) {
         applicationEventPublisher.publishEvent(event)
     }
