d679c26c67a991067dc988e796d0af615a1e5bf8
JaeUk <siwall0105@gmail.com>
2024-10-28 16:44:24 +0900

    docs(schema.sql): #18 프로모션 관련 table schema 정의

--- damaba/src/main/resources/schema.sql
+++ damaba/src/main/resources/schema.sql
@@ -1,5 +1,9 @@
 DROP TABLE IF EXISTS `user`;
 DROP TABLE IF EXISTS user_profile_image;
+DROP TABLE IF EXISTS promotion;
+DROP TABLE IF EXISTS promotion_image;
+DROP TABLE IF EXISTS promotion_active_region;
+DROP TABLE IF EXISTS promotion_hashtag;
 
 CREATE TABLE `user`
 (
@@ -22,7 +26,7 @@ CREATE TABLE user_profile_image
 (
     id         BIGINT       NOT NULL AUTO_INCREMENT,
     user_id    BIGINT       NOT NULL COMMENT '(FK) id of user',
-    name       VARCHAR(255) NOT NULL,
+    name       VARCHAR(255) NOT NULL UNIQUE,
     url        VARCHAR(255) NOT NULL UNIQUE,
     created_at DATETIME     NOT NULL,
     updated_at DATETIME     NOT NULL,
@@ -32,3 +36,64 @@ CREATE TABLE user_profile_image
     PRIMARY KEY (id)
 );
 CREATE INDEX idx__user_profile_image__url ON user_profile_image (url);
+
+CREATE TABLE promotion
+(
+    id                        BIGINT       NOT NULL AUTO_INCREMENT,
+    author_id                 BIGINT COMMENT '(FK) id of user(author)',
+    type                      VARCHAR(255) NOT NULL,
+    event_type                VARCHAR(255),
+    title                     VARCHAR(20)  NOT NULL,
+    content                   VARCHAR(500) NOT NULL,
+    sido                      VARCHAR(255) NOT NULL,
+    sigungu                   VARCHAR(255) NOT NULL,
+    road_address              VARCHAR(255),
+    jibun_address             VARCHAR(255),
+    external_link             VARCHAR(255),
+    started_at                DATE,
+    ended_at                  DATE,
+    photographer_name         VARCHAR(255),
+    photographer_instagram_id VARCHAR(255),
+    created_at                DATETIME     NOT NULL,
+    updated_at                DATETIME     NOT NULL,
+    deleted_at                DATETIME,
+    created_by                BIGINT       NOT NULL,
+    updated_by                BIGINT       NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE promotion_image
+(
+    id           BIGINT       NOT NULL AUTO_INCREMENT,
+    promotion_id BIGINT       NOT NULL COMMENT '(FK) id of promotion',
+    name         VARCHAR(255) NOT NULL UNIQUE,
+    url          VARCHAR(255) NOT NULL UNIQUE,
+    created_at   DATETIME     NOT NULL,
+    updated_at   DATETIME     NOT NULL,
+    created_by   BIGINT       NOT NULL,
+    updated_by   BIGINT       NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE promotion_active_region
+(
+    id           BIGINT       NOT NULL AUTO_INCREMENT,
+    promotion_id BIGINT       NOT NULL COMMENT '(FK) id of promotion',
+    category     VARCHAR(255) NOT NULL,
+    name         VARCHAR(255) NOT NULL,
+    created_at   DATETIME     NOT NULL,
+    updated_at   DATETIME     NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE INDEX fk_idx__promotion_active_region__promotion_id ON promotion_active_region (promotion_id);
+
+CREATE TABLE promotion_hashtag
+(
+    id           BIGINT       NOT NULL AUTO_INCREMENT,
+    promotion_id BIGINT       NOT NULL COMMENT '(FK) id of promotion',
+    content      VARCHAR(255) NOT NULL,
+    created_at   DATETIME     NOT NULL,
+    updated_at   DATETIME     NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE INDEX fk_idx__promotion_hashtag__promotion_id ON promotion_hashtag (promotion_id);
