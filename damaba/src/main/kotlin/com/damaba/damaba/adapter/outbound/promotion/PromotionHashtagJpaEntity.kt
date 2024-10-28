b4ec7b997c446195f4cf46ec9ef2a781d65cc313
JaeUk <siwall0105@gmail.com>
2024-10-28 16:47:46 +0900

    feat(damaba): #18 프로모션 domain entity, jpa entity 구현

---
+++ damaba/src/main/kotlin/com/damaba/damaba/adapter/outbound/promotion/PromotionHashtagJpaEntity.kt
@@ -0,0 +1,43 @@
+package com.damaba.damaba.adapter.outbound.promotion
+
+import com.damaba.damaba.adapter.outbound.common.BaseJpaTimeEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+
+@Table(name = "promotion_hashtag")
+@Entity
+class PromotionHashtagJpaEntity(
+    promotion: PromotionJpaEntity,
+    content: String,
+) : BaseJpaTimeEntity() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    val id: Long = 0
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "promotion_id", nullable = false)
+    var promotion: PromotionJpaEntity = promotion
+        private set
+
+    @Column(name = "content", nullable = false)
+    var content: String = content
+        private set
+
+    companion object {
+        fun from(
+            hashtagContent: String,
+            promotionJpaEntity: PromotionJpaEntity,
+        ): PromotionHashtagJpaEntity = PromotionHashtagJpaEntity(
+            promotion = promotionJpaEntity,
+            content = hashtagContent,
+        )
+    }
+}
---
+++ damaba/src/main/kotlin/com/damaba/damaba/domain/promotion/PromotionImage.kt
@@ -0,0 +1,6 @@
+package com.damaba.damaba.domain.promotion
+
+data class PromotionImage(
+    val name: String,
+    val url: String,
+)
