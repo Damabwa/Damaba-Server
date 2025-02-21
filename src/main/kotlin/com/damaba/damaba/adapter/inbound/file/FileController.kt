ad9d234a560a4a8b6d2b06580c41b05d53143b18
JaeUk <siwall0105@gmail.com>
2025-02-21 22:41:17 +0900

    docs: 파일 업로드 API 명세에 설명 추가

--- src/main/kotlin/com/damaba/damaba/adapter/inbound/file/FileController.kt
+++ src/main/kotlin/com/damaba/damaba/adapter/inbound/file/FileController.kt
@@ -18,6 +18,9 @@ import org.springframework.web.bind.annotation.RestController
 class FileController(private val uploadFilesUseCase: UploadFilesUseCase) {
     @Operation(
         summary = "Upload files",
+        description = "<p>파일을 업로드합니다." +
+            "<p>요청 시, 업로드하는 파일의 성격에 맞는 <code>fileType</code>을 설정해야 합니다." +
+            "<p><strong>만약 <code>fileType</code>에 적절한 항목이 없다면 반드시 서버 관리자에게 문의 부탁드립니다.</strong>",
         security = [SecurityRequirement(name = "access-token")],
     )
     @PostMapping("/api/v1/files", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
