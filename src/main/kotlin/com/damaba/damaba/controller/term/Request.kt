f67542c0c1f5073961924d34f62945f5bd586543
yoonseo8167 <yoonseo8167@sookmyung.ac.kr>
2025-08-06 19:32:35 +0900

    refactor: #164 불필요 dto 삭제

--- src/main/kotlin/com/damaba/damaba/controller/term/Request.kt
+++ src/main/kotlin/com/damaba/damaba/controller/term/Request.kt
@@ -1,8 +1,7 @@
+@file:Suppress("ktlint:standard:filename")
+
 package com.damaba.damaba.controller.term
 
-import com.damaba.damaba.application.term.AcceptPhotographerTermsCommand
-import com.damaba.damaba.application.term.AcceptUserTermsCommand
-import com.damaba.damaba.application.term.TermItem
 import com.damaba.damaba.domain.term.TermType
 import io.swagger.v3.oas.annotations.media.Schema
 
@@ -13,20 +12,3 @@ data class AgreementRequestItem(
     @Schema(description = "사용자의 동의 여부")
     val agreed: Boolean,
 )
-
-data class UserTermRequest(
-    @Schema(
-        description = "사용자가 동의한 약관 항목들",
-    )
-    val agreements: List<AgreementRequestItem>,
-) {
-    fun toUserCommand(userId: Long): AcceptUserTermsCommand = AcceptUserTermsCommand(
-        userId = userId,
-        terms = agreements.map { TermItem(it.type, it.agreed) },
-    )
-
-    fun toPhotographerCommand(userId: Long): AcceptPhotographerTermsCommand = AcceptPhotographerTermsCommand(
-        userId = userId,
-        terms = agreements.map { TermItem(it.type, it.agreed) },
-    )
-}
