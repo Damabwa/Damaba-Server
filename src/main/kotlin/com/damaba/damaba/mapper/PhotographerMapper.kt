caa795e0fa3986cdce8f13883c1cfbd65b1da291
JaeUk <siwall0105@gmail.com>
2025-09-19 01:24:33 +0900

    fix: 사진작가 응답 중 촬영 종류, 활동 지역에 대해 순서가 항상 일정하도록 수정

--- src/main/kotlin/com/damaba/damaba/mapper/PhotographerMapper.kt
+++ src/main/kotlin/com/damaba/damaba/mapper/PhotographerMapper.kt
@@ -3,17 +3,25 @@ package com.damaba.damaba.mapper
 import com.damaba.damaba.application.photographer.UpdatePhotographerPageCommand
 import com.damaba.damaba.application.photographer.UpdatePhotographerProfileCommand
 import com.damaba.damaba.controller.photographer.PhotographerResponse
+import com.damaba.damaba.controller.region.RegionResponse
 import com.damaba.damaba.domain.photographer.Photographer
 import com.damaba.damaba.domain.photographer.PhotographerListItem
 import com.damaba.damaba.domain.photographer.PhotographerListItemResponse
 import com.damaba.damaba.domain.photographer.PhotographerPage
 import com.damaba.damaba.domain.photographer.PhotographerProfile
+import com.damaba.damaba.domain.region.Region
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
+import org.mapstruct.Named
 import org.mapstruct.factory.Mappers
 
-@Mapper(uses = [AddressMapper::class, ImageMapper::class])
+@Mapper(uses = [AddressMapper::class, ImageMapper::class, RegionMapper::class])
 interface PhotographerMapper {
+    @Mapping(
+        target = "mainPhotographyTypes",
+        expression = "java(new java.util.LinkedHashSet<>(photographer.getMainPhotographyTypes()))",
+    )
+    @Mapping(target = "activeRegions", source = "activeRegions", qualifiedByName = ["regionsToLinkedHashSet"])
     fun toPhotographerResponse(photographer: Photographer): PhotographerResponse
 
     @Mapping(source = "saved", target = "isSaved")
@@ -24,6 +32,11 @@ interface PhotographerMapper {
     fun toPhotographerPage(command: UpdatePhotographerPageCommand): PhotographerPage
 
     fun toPhotographerListItem(photographer: Photographer, isSaved: Boolean): PhotographerListItem
+
+    @Named("regionsToLinkedHashSet")
+    fun regionsToLinkedHashSet(regions: Set<Region>): LinkedHashSet<RegionResponse> = regions.stream()
+        .map { region -> RegionMapper.INSTANCE.toRegionResponse(region) }
+        .collect(java.util.stream.Collectors.toCollection { LinkedHashSet() })
 
     companion object {
         val INSTANCE: PhotographerMapper = Mappers.getMapper(PhotographerMapper::class.java)
